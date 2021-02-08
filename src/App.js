4a6084b05268720d7cf6c3477b927ba2ef6092dd
kellynauert <kellynauert@gmail.com>
2021-02-07 20:16:28 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Button } from 'reactstrap';
 import Jobs from './components/jobs/Jobs';
 
@@ -15,6 +14,18 @@ function App() {
 		localStorage.setItem('Longitude', newPosition.coords.longitude);
 		setSessionLongitude(newPosition.coords.longitude);
 	};
+
+	useEffect(() => {
+		if (localStorage.getItem('Latitude')) {
+			setSessionLatitude(localStorage.getItem('Latitude'));
+		}
+	});
+
+	useEffect(() => {
+		if (localStorage.getItem('Longitude')) {
+			setSessionLongitude(localStorage.getItem('Longitude'));
+		}
+	});
 
 	function getLocation() {
 		if (navigator.geolocation) {
